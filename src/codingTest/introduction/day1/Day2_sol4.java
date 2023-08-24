ec92dd6c7aea3f47f11b48a5e9076127366089fa
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-08-24 18:41:59 +0900

    2023.08.24 프로그래머스 코딩테스트 입문 Day2 문제4

--- src/codingTest/introduction/day1/Day2_sol4.java
+++ src/codingTest/introduction/day1/Day2_sol4.java
@@ -1,5 +1,14 @@
 package codingTest.introduction.day1;
 
 public class Day2_sol4 {
+	
+    public int[] solution(int[] numbers) {
+        
+        for(int i=0; i<numbers.length; i++) {
+            numbers[i] = numbers[i]*2;
+        }
+
+        return numbers;
+    }
 
 }
