0808082fe588e523d5775b26beb00834c7c67270
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2023-11-21 15:06:54 +0900

    [level 1] Title: 추억 점수, Time: 2.82 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        
+        Map<String, Integer> hMap = new HashMap<>();
+        
+        // 각 이름별로 그리움 점수를 Map에 담기
+        for(int i=0;i<yearning.length;i++) {
+            hMap.put(name[i],yearning[i]);
+            // 그리움 점수가 없는 사람은 Map에 담기지 않게 된다.
+        }
+        
+        // 각 배열 별 그리움 점수 구하기
+        List<Integer> list = new ArrayList<>();
+        for(String[] p : photo) {
+            int sum = 0;
+            for(int i=0;i<p.length;i++){
+                sum += hMap.getOrDefault(p[i],0);
+                // 그리움 점수가 없으면 0이 더해지도록 하기!
+            }
+            list.add(sum);
+        }
+        
+        answer = list.stream().mapToInt(i -> i).toArray();
+        
+        return answer;
+    }
+}
