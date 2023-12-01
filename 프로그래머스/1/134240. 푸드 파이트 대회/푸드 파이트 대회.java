b122015e689ea497a50e3db6a51e746742c3dd0e
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2023-12-01 20:22:04 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.92 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,24 @@
+// PM 8:13 시작
+// 1차 PM 8:21
+
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i=1;i<food.length;i++) {
+            for(int j=0;j<(food[i]/2);j++) {
+                answer += i;
+            }
+        }
+        
+        answer += 0;
+        
+        for(int i=food.length-1;i>0;i--) {
+            for(int j=0;j<(food[i]/2);j++) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
