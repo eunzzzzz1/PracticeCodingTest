1a5fae5d771fc9c4ef5c23705277adf1cf69c51c
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2023-12-12 22:10:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.35 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,33 @@
+// PM 9:38 시작
+// 1차 PM 10:07
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int lth = score.length;
+        int[] answer = new int[lth];
+    
+        answer[0] = score[0];
+        
+        for(int i=1; i<lth; i++) {
+            
+            if(i<k) {
+                answer[i] = Math.min(answer[i-1],score[i]);
+                
+            } else {
+                
+                int[] arr = new int[i+1];
+                for(int j=0;j<i+1;j++) {
+                    arr[j] = score[j];
+                }
+                
+                Arrays.sort(arr);
+                answer[i] = arr[i-k+1];
+            }
+        }
+        return answer;
+        
+        
+    }
+}
