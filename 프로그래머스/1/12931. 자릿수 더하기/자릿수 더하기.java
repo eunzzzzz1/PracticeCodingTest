3ecaddc9c856c6f77e8f5e8a1dbb1d4de59f0656
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2023-12-18 18:12:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int length = Integer.toString(n).length();
+
+        for(int i=0; i<length;i++) {
+            answer += n%10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
