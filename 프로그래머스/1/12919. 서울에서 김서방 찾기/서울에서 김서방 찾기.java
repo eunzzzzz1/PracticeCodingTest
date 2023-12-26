dadf63429716a6e0300b11676b9b4b5ab42ccafb
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2023-12-26 23:11:59 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.94 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,10 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
         
-        for(int i=0;i<seoul.length;i++) {
-            if(seoul[i].equals("Kim")) {
-                return "김서방은 " + i + "에 있다";
-            }
-        }
+        List<String> list = Arrays.asList(seoul);
         
-        return "";
+        return "김서방은 " + list.indexOf("Kim") + "에 있다";
     }
 }
