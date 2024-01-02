b52b61419813a69f34d0630007a2b7587350c444
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-01-03 08:24:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.80 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,33 @@
+// 시작 AM 8:04
+// 1차 AM 8:17
+
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        if(n<m) {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+        
+        // 최대공약수 - 유클리드 호제법 이용하기
+        int b = m;
+        int r = n%m;
+        
+        while(r!=0) {
+            int temp = b;
+            b=r;
+            r=temp%r;
+            System.out.println("r : " + r);
+        }
+        
+        answer[0] = b;
+        System.out.println("b : " + b);
+        
+        // 최소공배수
+        answer[1] = b * (n/b) * (m/b);
+        
+        return answer;
+    }
+}
