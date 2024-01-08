0bca22a21c6972adc6edc7f906aff7e878250b61
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-01-08 14:54:23 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.29 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -14,39 +14,9 @@ class Solution {
             }
         }
         
-        int[] answer = hSet.stream().mapToInt(i -> i).toArray();
-        sort(answer, 0, answer.length-1);
+        int[] answer = hSet.stream().sorted().mapToInt(i -> i).toArray();
         
         return answer;
         
     }
-    
-    // 퀵정렬 메소드 다시 만들어보자!
-    void sort (int[] arr, int start, int end) {
-        
-        int pl = start;
-        int pr = end;
-        int x = arr[(pl+pr)/2];
-        
-        do {
-            
-            while(arr[pl]<x) pl++;
-            while(arr[pr]>x) pr--;
-            
-            if(pl<=pr) {
-                int temp = arr[pr];
-                arr[pr] = arr[pl];
-                arr[pl] = temp;
-                
-                pl++;
-                pr--;
-            }
-
-        } while (pl<=pr);
-        
-        if(pl<end) sort(arr,pl,end);
-        if(pr>start) sort(arr,start,pr);
-
-    }
-    
 }
