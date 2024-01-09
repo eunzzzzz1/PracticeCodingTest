6a8ab31f4a4e3f01328ed6ca85bf5b9ec014b53e
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-01-09 14:47:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.12 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -5,27 +5,17 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
 
         String[] answer = new String[n];
-        StringBuffer sb = new StringBuffer();
 
         for(int i=0; i<n; i++) {
             
-            String str1 = Integer.toBinaryString(arr1[i]);
-            String str2 = Integer.toBinaryString(arr2[i]);
+            String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            str = String.format("%16s", str)
+                .substring(16-n)
+                .replaceAll("1","#")
+                .replaceAll("0"," ");
             
-            while(str1.length()!=n) str1 = "0" + str1;
-            while(str2.length()!=n) str2 = "0" + str2;
+            answer[i] = str;
 
-            for(int j=0; j<n; j++) {
-
-                if(str1.charAt(j)=='1' || str2.charAt(j)=='1')
-                    sb.append("#");
-                else
-                    sb.append(" ");
-                
-            }
-            
-            answer[i] = sb.toString();
-            sb.delete(0, n);
         }
 
         return answer;
