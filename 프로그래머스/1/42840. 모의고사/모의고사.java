044bece1fe32d6efc4da26d3a8f83e1583bacdc2
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-01-10 21:33:36 +0900

    [level 1] Title: 모의고사, Time: 2.70 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,26 +6,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         
-        Map<Integer, Integer> hMap = new HashMap<>();
-        hMap.put(1, 0);
-        hMap.put(2, 0);
-        hMap.put(3, 0);
+        int[] score = new int[3];
         
         int[] arr1 = {2,1,2,3,2,4,2,5};
         int[] arr2 = {3,3,1,1,2,2,4,4,5,5};
         
         for (int i=0; i<answers.length; i++) {
-            if((i%5+1)==answers[i]) hMap.put(1, hMap.get(1)+1);
-            if(arr1[i%arr1.length] == answers[i]) hMap.put(2, hMap.get(2)+1);
-            if(arr2[i%arr2.length] == answers[i]) hMap.put(3, hMap.get(3)+1);
+            if((i%5+1)==answers[i]) score[0]++;
+            if(arr1[i%arr1.length] == answers[i]) score[1]++;
+            if(arr2[i%arr2.length] == answers[i]) score[2]++;
         }
         
-        int max = Math.max(hMap.get(1), Math.max(hMap.get(2), hMap.get(3)));
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
         
         List<Integer> list = new ArrayList<>();
         
-        for(int n : hMap.keySet()) {
-            if(hMap.get(n)==max) list.add(n);
+        for(int i=1; i<=3; i++) {
+            if(score[i-1]==max) list.add(i);
         }
         
         return list.stream().mapToInt(i -> i).toArray();
