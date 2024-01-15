6f9b5f2c1add14f222e8c55d5944bc80f55abd1e
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-01-15 15:00:18 +0900

    [level Lv.1] Title: 체육복, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/42862. 체육복/체육복.java
+++ 프로그래머스/Lv.1/42862. 체육복/체육복.java
@@ -1,45 +1,40 @@
 // 시작 PM 1:23
 // 1차 PM 1:41
 // 2차 PM 1:56
+// 3차 PM 2:18
 
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         
-        int answer = n - lost.length;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        int answer = n;
+        int[] arr = new int[n];
         
-        int[] lostArr = new int[n+2];
         for(int i : lost) {
-            lostArr[i] ++;
+            arr[i-1]--;
         }
-
-        for(int i=0; i<reserve.length; i++) {
-            
-            int j = reserve[i];
-            
-            if(lostArr[j]==1) {
-                answer++;
-                lostArr[j] = 0;
-                reserve[i] = 0;
-            }
-        }
-
         
-        for(int j : reserve) {
+        for(int i : reserve) {
+            arr[i-1]++;
+        }
 
-            if(j==0) continue;
+        for(int i=0; i<n; i++) {
             
-            if(lostArr[j-1]==1) {
-                answer++;
-                lostArr[j-1] = 0;
-            } else if(lostArr[j+1]==1) {
-                answer ++;
-                lostArr[j+1] = 0;
+            if(arr[i] == -1) {
+                            
+                if (i-1>=0 && arr[i-1]==1) {
+                    arr[i-1]--;
+                    arr[i]++;
+                } else if (i+1<n && arr[i+1]==1) {
+                    arr[i]++;
+                    arr[i+1]--;
+                } else {
+                    answer--;
+                }
+                
             }
-            
+
         }
         
         return answer;
