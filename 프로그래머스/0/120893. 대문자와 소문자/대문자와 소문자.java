43273c91ff3df06b1d5818efb3f8cf59c94bafa2
eunzzzzz1 <139827141+eunzzzzz1@users.noreply.github.com>
2024-02-23 16:47:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.07 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -3,8 +3,14 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(String my_string) {
-        return my_string.chars()
-            .mapToObj(i->(i>='A'&&i<='Z')?String.valueOf((char)(i+32)):String.valueOf((char)(i-32)))
-            .collect(Collectors.joining());
+        
+        char[] arr = my_string.toCharArray();
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]>='A' && arr[i]<='Z') arr[i] = (char)(arr[i]+32);
+            else arr[i] = (char)(arr[i]-32);
+        }
+        
+        return new String(arr);
     }
 }
