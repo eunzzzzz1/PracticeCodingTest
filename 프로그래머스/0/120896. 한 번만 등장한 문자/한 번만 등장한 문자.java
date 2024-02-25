ff1c1ebc49dc3db38d711a108c71d37a7e09d613
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-02-25 18:26:07 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 5.64 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -3,22 +3,14 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(String s) {
-        Map<Character, Integer> hMap = new LinkedHashMap<>();
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
         
-        for(char a : arr) {
-            hMap.put((char)a, hMap.getOrDefault((char)a, 0)+1);
-        }
-        
-        StringBuffer sb = new StringBuffer();
-        
-        for(char a : hMap.keySet()) {
-            if(hMap.get(a)==1) sb.append((char)a);
-        }
-        
-        if(sb.length()==0) return "";
-
-        return sb.toString();
+        return Arrays.stream(s.split(""))
+            .collect(Collectors.groupingBy(i -> i))
+            .entrySet()
+            .stream()
+            .filter(i -> i.getValue().size() == 1)
+            .map(Map.Entry::getKey)
+            .sorted()
+            .collect(Collectors.joining());
     }
 }
