a113a00448b5dc16e45be3bffd2b576c23adaebd
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-02-26 10:18:10 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String my_string) {
+        String[] arr = my_string.split(" ");
+        int answer = Integer.parseInt(arr[0]);
+        
+        for(int i=1; i<arr.length; i=i+2) {
+            switch(arr[i]) {
+                case "+" :
+                    answer += Integer.parseInt(arr[i+1]);
+                    break;
+                case "-" :
+                    answer -= Integer.parseInt(arr[i+1]);
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
