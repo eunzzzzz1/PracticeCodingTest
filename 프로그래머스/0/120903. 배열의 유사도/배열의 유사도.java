0042d72ab1c10480879a6b3e300f11dfcb109a0d
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-02-26 10:28:26 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        for(String s : s1) {
+            for(String t : s2) {
+                if(s.equals(t)) answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
