f05d91d282f8374b0ac3b7db43c9b487ac285dda
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-02-26 09:59:46 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        for(int i=0; i<array.length; i++) {
+            answer[0] = Math.max(answer[0], array[i]);
+            if(answer[0] == array[i]) answer[1] = i;
+        }
+        return answer;
+    }
+}
