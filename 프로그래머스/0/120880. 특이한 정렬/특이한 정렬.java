454a3b71ff547f1df173dd30a389a37e8225b792
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-03-06 18:52:11 +0900

    [level 0] Title: 특이한 정렬, Time: 1.93 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i=0; i<numlist.length; i++) {
+            if(i==0) {
+                answer.add(numlist[i]);
+                continue;
+            }
+            
+            for(int j=0; j<answer.size(); j++) {
+                if(Math.abs(answer.get(j)-n) > Math.abs(numlist[i]-n)) {
+                    answer.add(j, numlist[i]);
+                    break;
+                } else if (Math.abs(answer.get(j)-n) == Math.abs(numlist[i]-n)
+                            && answer.get(j) < numlist[i]) {
+                    answer.add(j, numlist[i]);
+                    break;
+                }
+            }
+            
+            if(answer.indexOf(numlist[i])==-1) answer.add(numlist[i]);
+
+        }
+        
+    
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
