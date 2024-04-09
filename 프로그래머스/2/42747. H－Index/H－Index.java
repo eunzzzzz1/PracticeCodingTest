bd178f79405892a3822be81e0b3fb3393cbba183
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-04-09 17:46:02 +0900

    [level 2] Title: H-Index, Time: 0.52 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+        
+        for(int i=0; i<n; i++) {
+            answer = Math.max(answer, Math.min(citations[i], n-i));
+        }
+        
+        return answer;
+    }
+}
