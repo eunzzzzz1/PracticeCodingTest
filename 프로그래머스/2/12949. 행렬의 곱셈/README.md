95624088b889b7f0692b00f7aef3698dafd93b37
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-04-15 14:15:35 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.56 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 1.42 ms
+메모리: 78.3 MB, 시간: 1.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 14:05:44
+2024년 04월 15일 14:15:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int n = arr1.length;
-        int m = arr2[0].length;
-        int l = arr2.length;
-        int[][] answer = new int[n][m];
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                for(int k=0; k<l; k++) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j];
+        int m = arr1.length;
+        int k = arr2[0].length;
+        int n = arr2.length;
+
+        int[][] answer = new int[m][k];
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<k; j++) {
+                for(int l=0; l<n; l++) {
+                    answer[i][j] += arr1[i][l] * arr2[l][j];
                 }
             }
         }
-        
+
         return answer;
     }
 }
