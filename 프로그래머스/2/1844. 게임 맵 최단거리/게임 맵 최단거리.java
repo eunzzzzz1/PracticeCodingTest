9f7a22ea54db4511e00646f98edb22e973ce285e
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-05-19 21:44:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.79 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,5 +1,3 @@
-// PM 7:07 시작
-
 import java.util.*;
 
 class Solution {
@@ -13,26 +11,28 @@ class Solution {
         n = maps.length;
         m = maps[0].length;
         this.maps = maps;
-        
-        // 진영이 가로막혀서 갈 수 없을 때!
 
         int answer = bfs(0,0);
         return (answer == 1) ? -1 : answer;
+        // answer이 1이면 방문할 수 없다는 뜻 => -1을 return 한다.
     }
     
     private int bfs(int x, int y) {
         
+        // 상, 하, 좌, 우를 살펴주기 위한 배열
         int[] dx = {-1, 1, 0, 0}; // 행 이동
         int[] dy = {0, 0, -1, 1}; // 열 이동
 
         Queue q = new LinkedList();
-        int[] a = {x, y};
+        // Queue에는 현재 방문한 좌표로부터
+        // 인접한 노드들 중, 방문하지 않은 노드를 넣어줄 것.
         
-        q.add(a);
+        int[] a = {x, y}; // 시작점
+        q.add(a); // Queue에 넣어준다.
         
         while(!q.isEmpty()) {
             
-            int[] b = (int[])q.poll();
+            int[] b = (int[])q.poll(); // 노드를 꺼내준다.
             
             x = b[0];
             y = b[1];
@@ -43,16 +43,16 @@ class Solution {
                 int ny = y + dy[i];
                 
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue; // 범위 벗어남
-                else if(maps[nx][ny] == 1) { // 새롭게 들린 길이면
+                // else if(maps[nx][ny] == 0) continue;
+                else if(maps[nx][ny] == 1) { // 인접 노드가 방문하지 않은 노드라면
                     int[] c = {nx, ny};
                     q.add(c); // Queue에 넣어준다.
-                    maps[nx][ny] = maps[x][y] + 1; // 지금까지 온 길 + 1
+                    maps[nx][ny] = maps[x][y] + 1;
+                    	// 시작 좌표부터 (nx, ny) 까지 이동한 칸 수를
+                        // 점점 누적해서 더해준다.
+                        // ( 방문 여부 체크가 되기도 한다. )
                 }
-                
-                // maps[nx][ny] 가 1이 아닌 경우 = 이미 들린 길이므로
-                // 되돌아가지 않는다.
             }
-            
         }
         
         return maps[n-1][m-1];
