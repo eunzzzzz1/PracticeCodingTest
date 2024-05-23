cee02f5e891ba8d826a838b5dc9eb95e5d31419f
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-05-23 11:58:21 +0900

    [level 2] Title: 주식가격, Time: 31.69 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,12 +3,27 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Stack<Integer> s = new Stack<>();
         
-        for(int i=0; i<prices.length-1; i++) {
-            for(int j=i+1; j<prices.length; j++) {
-                answer[i]++;
-                if(prices[i]>prices[j]) break; 
+        // prices를 순회하면서, 차례로 stack에 인덱스를 담아둔다.
+        // stack에 남아있는 인덱스들의 값과 prices[i]를 비교해보았을 때,
+        // prices[i] 값이 더 작다면 (=값이 떨어졌다면) 인덱스간의 차를 통해 지속시간을 계산한다.
+        // stack 에는 prices[i] 보다 작은 값들의 인덱스들만이 담기게 된다. (값이 떨어지지않은 것들)
+        
+        int i = 0;
+        s.push(i);
+        
+        for(i=0; i<prices.length; i++) {
+            while(!s.isEmpty() && prices[i] < prices[s.peek()]) {
+                int temp = s.pop();
+                answer[temp] = i - temp;
             }
+            s.push(i);
+        }
+        
+        while(!s.isEmpty()) {
+            int temp = s.pop();
+            answer[temp] = i - 1 - temp;
         }
         
         return answer;
