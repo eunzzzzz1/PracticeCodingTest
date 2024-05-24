906ce365d53325b4fd69da5a3ca27cedddf9dd62
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-05-24 14:08:57 +0900

    [level 2] Title: 더 맵게, Time: 1314.50 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] scoville, int K) {
+        int i = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 작은 숫자가 가장 먼저 나옴
+        
+        for(int s : scoville) {
+            pq.add(s);
+        }
+        
+        while(pq.peek() < K && pq.size() > 1) {
+            int temp = pq.poll() + pq.poll() * 2;
+            pq.add(temp);
+            i++;
+            if(pq.size() == 1 && pq.peek() < K) return -1;
+        }
+
+        return i;
+    }
+
+}
