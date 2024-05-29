b7d391869b8532c9a552a10bc94e164cda7e612b
김은지 Eunji <139827141+eunzzzzz1@users.noreply.github.com>
2024-05-29 12:18:22 +0900

    [level 2] Title: 롤케이크 자르기, Time: 11.78 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        int[] arr1 = new int[10000];
+        int[] arr2 = new int[10000];
+        
+        int max = 0;
+        
+        for(int a : topping) {
+            arr1[a-1]++;
+            max = Math.max(a, max);
+        }
+        
+        int n = 0;
+        int m = 0;
+        
+        for(int i=0; i<max; i++) {
+            if(arr1[i] != 0) n++;
+        }
+
+        for(int a : topping) {
+            
+            int temp = arr2[a-1];
+            
+            arr1[a-1]--;
+            arr2[a-1]++;
+            
+            if(arr1[a-1] == 0) n--;
+            if(temp == 0 && arr2[a-1] != 0) m++;
+            
+            if(n==m) answer++;
+            if(n<m) break;
+        }
+        
+        return answer;
+    }
+}
